2a3f38a8fc4e76bec390c41a37b5ff6350c2d245
Digamber <singhdigrana@outlook.com>
2020-01-16 17:48:19 +0530

    Updated a Holidaylist file.

--- vmstart.ps1
+++ vmstart.ps1
@@ -1,6 +1,7 @@
 <#
     .DESCRIPTION
-        Start all the VM's scheduled to be sstarted in all resource groups using the Run As Account (Service Principle)
+        Start all the VM's scheduled to start using the Run As Account (Service Principle)
+        Before starting the VM, check if today is not holiday in your organisation.
 
     .NOTES
         AUTHOR: Azure Automation Team
@@ -30,14 +31,21 @@ catch {
     }
 }
 $todaysdate = Get-Date -DisplayHint Date
-$holiday = Get-Date -Day 5 -Hour - -Minute 0 -Second 0 -DisplayHint Date
-# Get reference to each VM with tag scheduedstart=yes value and start the VM
-$vms = Get-AzureRmResource | Where-Object { $_.ResourceType -eq "Microsoft.Compute/virtualMachines" -and $_.Tags.Values } 
-ForEach ($vm in $vms) {          
-    if ($vm.Tags.Name -eq "scheduledstart" -and $vm.Tags.Value -eq "Yes") {
-        if ($todaysdate -ne $holiday) {
-            Start-AzureRmVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name
-            Write-Output ($vm.Name + " Virtual Machine started successfully!") 
-        }            
-    }        
+$holidaylist = Get-Content "Holidaylist.txt"
+# $holiday = Get-Date -Day 5 -Hour - -Minute 0 -Second 0 -DisplayHint Date
+if (!$holidaylist.Contains($todaysdate.ToShortDateString())) {    
+    # Get reference to each VM with tag scheduedstart=yes value and start the VM
+    $vms = Get-AzureRmResource | Where-Object { $_.ResourceType -eq "Microsoft.Compute/virtualMachines" -and $_.Tags.Values } 
+    ForEach ($vm in $vms) {          
+        if ($vm.Tags.Name -eq "scheduledstart" -and $vm.Tags.Value -eq "Yes") {
+            if ($todaysdate -ne $holiday) {
+                Start-AzureRmVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name
+                Write-Output ($vm.Name + " Virtual Machine started successfully!") 
+            }            
+        }        
+    }
 }
+else {
+    Write-Host ("Today is Holiday, so Virtual Machine is not started!")
+}
+
