46a40933069c281b46ccc40240e70fb1e714eeec
Digamber <singhdigrana@outlook.com>
2020-01-17 19:02:25 +0530

    Updated the vmstartstop script

--- vmstartstop.ps1
+++ vmstartstop.ps1
@@ -1,7 +1,7 @@
 <#
     .DESCRIPTION
         Start/Stop all the VM's scheduled to start using the Run As Account (Service Principle)
-        Before Starting the VM, check if today is not holiday in your organisation.
+        Before Starting the VM, check if today is not holiday in your organisation. if holiday and vm is running stop it.
         Before Stopping the VM, Check if its not the first day of the Month, As first day of month is a busy day!
 
     .NOTES
@@ -33,18 +33,20 @@ catch {
 }
 $todaysdate = Get-Date -DisplayHint Date
 $firstDayOfMonth = Get-Date -Day 1 -Hour 0 -Minute 0 -Second 0 -DisplayHint Date
-
 $current_time = Get-Date -DisplayHint Time
-$morningStartTime = "9:00"
-$morningEndTime = "11:55"
+$morning_start_time = Get-AutomationVariable -Name 'morning_start_time'
+$morning_end_time = Get-AutomationVariable -Name 'morning_end_time'
 
-if ($current_time -ge $morningStartTime -and $current_time -le $morningEndTime) {
-    Write-Host("Its Morning-start the machine");
+#By Default it is taking UK time
+Write-Output(Get-Date)
+# Start the VM
+if ($current_time -ge $morning_start_time -and $current_time -le $morning_end_time) {        
+    Write-Output("Its Morning - start the virtual machine")
     $uri = "https://raw.githubusercontent.com/singhdigrana/azdesignpatterns/master/Holidaylist.txt"
     $webRequest = Invoke-WebRequest -uri $uri -UseBasicParsing
 
     $holidaylist = $webRequest.Content
-    $holidaylist = $holidaylist.Split(",")
+    $holidaylist = $holidaylist.Split(",")    
     $holiday = $holidaylist | ForEach-Object { [datetime]$_ }
     if (!($holiday -contains [datetime]::Today)) {   
         $vms = Get-AzureRmResource | Where-Object { $_.ResourceType -eq "Microsoft.Compute/virtualMachines" -and $_.Tags.Values } 
@@ -52,7 +54,7 @@ if ($current_time -ge $morningStartTime -and $current_time -le $morningEndTime) 
             if ($vm.Tags.Name -eq "startstop" -and $vm.Tags.Value -eq "True") {
                 if ($todaysdate -ne $holiday) {
                     Start-AzureRmVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name
-                    Write-Output ($vm.Name + " Virtual Machine started successfully!") 
+                    Write-Output ($vm.Name + " : Virtual Machine started successfully!") 
                 }            
             }        
         }
@@ -61,22 +63,22 @@ if ($current_time -ge $morningStartTime -and $current_time -le $morningEndTime) 
         Write-Output("Its Holiday! Virtual Machines can not be started!!")
         $vms = Get-AzureRmResource | Where-Object { $_.ResourceType -eq "Microsoft.Compute/virtualMachines" -and $_.Tags.Values } 
         ForEach ($vm in $vms) {          
-            
-            Stop-AzureRmVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -Force
-            Write-Output ($vm.Name + " Virtual Machine stopped successfully!") 
+            if ($vm.Tags.Name -eq "startstop" -and $vm.Tags.Value -eq "True") {
+                Stop-AzureRmVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -Force
+            }        
         }
     }
     $holidaylist = $null;
 }
 else {
     #Stop the Machine
-    Write-Host("Its Evening - stop the machine");    
-    # Get reference to each VM with tag scheduedstop=yes value and stop the VM
+    Write-Output("Its Night - Stop the virtual machine");    
     $vms = Get-AzureRmResource | Where-Object { $_.ResourceType -eq "Microsoft.Compute/virtualMachines" -and $_.Tags.Values } 
     ForEach ($vm in $vms) {          
         if ($vm.Tags.Name -eq "startstop" -and $vm.Tags.Value -eq "True") {
             if ($todaysdate -ne $firstDayOfMonth) {
                 Stop-AzureRmVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -Force
+                Write-Output ($vm.Name + " : Virtual Machine stopped successfully!")                 
             }
         }        
     }
